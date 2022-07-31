7ee71b68955feafd8148f11de91509d83012912c
riyalohia <riyalh1997@gmail.com>
2022-07-31 12:35:31 +0530

    Adds useState

---
+++ useStateCallback.js
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import useDidUpdate from './useDidUpdate'
+
+const useStateCallback = (initialState, f) => {
+  const [state, set] = useState(initialState)
+  useDidUpdate(f, [state])
+  return { state, set }
+}
+
+export default useStateCallback
