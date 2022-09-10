e10718a858053e9c7692383a15eb97795d196902
riyalohia <riyalh1997@gmail.com>
2022-09-10 13:00:45 +0530

    Adds useDefferedState

---
+++ useDeferredState.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+export const useDeferredState = (baseState, instantValues = [], defferFor = 500) => {
+  const [deffered, setDeffered] = React.useState(baseState);
+  const instantValuesRef = React.useRef(instantValues);
+
+  if (
+    instantValues.length !== instantValuesRef.current.length ||
+    instantValues.some((value, index) => instantValuesRef.current[index] !== value)
+  ) {
+    instantValuesRef.current = instantValues;
+  }
+
+  const memorizedInstantValues = instantValuesRef.current;
+
+  React.useLayoutEffect(() => {
+    const value = baseState;
+
+    if (deffered !== value) {
+      if (memorizedInstantValues.includes(value)) {
+        setDeffered(value);
+      } else {
+        const timeout = window.setTimeout(() => {
+          setDeffered(value);
+        }, defferFor);
+
+        return () => window.clearTimeout(timeout);
+      }
+    }
+  }, [baseState, defferFor, deffered, memorizedInstantValues]);
+
+  if (instantValues.includes(baseState)) {
+    return baseState;
+  }
+
+  return deffered;
+};
+
+export default useDeferredState;
